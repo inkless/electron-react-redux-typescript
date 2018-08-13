d1a428f29408ad0100702ad588d04abe407946ba
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2018-08-13 10:25:47 -0700

    add redux-thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
 
 import { rootReducer } from './reducers';
 import history from './history';
 
 function configureStore(initialState = {}) {
-  const middlewares: any[] = [routerMiddleware(history)];
+  const middlewares: any[] = [thunk, routerMiddleware(history)];
   const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
   return createStore(connectRouter(history)(rootReducer), initialState, enhancer);
 }
