15947ab9ef50439bef8df3837895eac4eb607bfc
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2018-10-05 21:50:15 -0700

    remove src/styles

--- src/styles/theme.ts
+++
@@ -1,9 +0,0 @@
-export interface ThemeInterface {
-  primaryColor: string;
-  primaryColorInverted: string;
-}
-
-export const theme = {
-  primaryColor: '#3f51b5',
-  primaryColorInverted: '#ffffff',
-};
